4a4ca5cfe213ee8888a8b67a37374068873d9053
Aderauwu04 <aderabg1@gmail.com>
2024-01-22 14:57:19 -0400

    Agregada estructura básica para asignar mas de un curso a un maestro guía

--- src/components/ConfigurarMaestro.vue
+++ src/components/ConfigurarMaestro.vue
@@ -29,6 +29,7 @@ const rols = ref([
 ])
 const rol = ref({id: 0, rol: 'Profesor'})
 const jornadas= ref(1)
+const cursos= ref(1)
 </script>
 <template>
 <v-dialog max-width="850" close-on-back scrollable :fullscreen="!smAndUp">
@@ -43,17 +44,17 @@ const jornadas= ref(1)
 
   <template v-slot:default="{ isActive }">
     <v-toolbar dark>
-        <v-toolbar-title>
-          {{ asignarRol ? 'Asignar rol' : 'Configurar' }}
-        </v-toolbar-title>
-        <v-toolbar-items>
-          <v-btn
-            icon="mdi-close"
-            dark
-            @click="isActive.value = false"
-          />
-        </v-toolbar-items>
-      </v-toolbar>
+      <v-toolbar-title>
+        {{ asignarRol ? 'Asignar rol' : 'Configurar' }}
+      </v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn
+          icon="mdi-close"
+          dark
+          @click="isActive.value = false"
+        />
+      </v-toolbar-items>
+    </v-toolbar>
     <v-card>
       <v-card-text v-if="asignarRol">
         <v-row>
@@ -68,36 +69,49 @@ const jornadas= ref(1)
               item-value="id"
             />
           </v-col>
-          <template v-if="rol.id == 2">
-            <v-col cols="12" md="6">
-              <v-combobox
-                chips
-                label="Mención"
-                :items="Object.values(materias)"
-                item-title="materia"
-                item-value="id"
-              />
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-combobox
-                chips
-                label="Año"
-                :items="Object.values(materias)"
-                item-title="materia"
-                item-value="id"
-              />
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-combobox
-                chips
-                label="Sección"
-                :items="Object.values(materias)"
-                item-title="materia"
-                item-value="id"
-              />
-            </v-col>
-          </template>
         </v-row>
+        <template v-if="rol.id == 2">
+          <section v-for="curso in cursos" :key="curso">
+            <v-divider/>
+            <p class="d-flex align-center">
+              <span class="flex-fill">
+                {{ curso }} curso asignado
+              </span>
+              <v-btn icon="mdi-trash-can" color="error" variant="text"/>
+            </p>
+            <v-divider class="mb-2"/>
+            <v-row>
+              <v-col cols="12" md="4">
+                <v-combobox
+                  chips
+                  label="Mención"
+                  :items="Object.values(materias)"
+                  item-title="materia"
+                  item-value="id"
+                />
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-combobox
+                  chips
+                  label="Año"
+                  :items="Object.values(materias)"
+                  item-title="materia"
+                  item-value="id"
+                />
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-combobox
+                  chips
+                  label="Sección"
+                  :items="Object.values(materias)"
+                  item-title="materia"
+                  item-value="id"
+                />
+              </v-col>
+            </v-row>
+          </section>
+          <v-btn block color="secundario" @click="cursos++" text="Asignar otra sección" />
+        </template>
       </v-card-text>
       <v-card-text v-else>
         <section v-for="jornada in jornadas">
