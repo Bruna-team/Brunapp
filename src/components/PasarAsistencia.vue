4df081e12fbb803b9817de9680f42d36e83a1d43
jessra <89423609+jessra@users.noreply.github.com>
2024-01-26 09:39:09 -0400

    ajustes de toma de asistenca

--- src/components/PasarAsistencia.vue
+++ src/components/PasarAsistencia.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, watch } from 'vue';
 import { brunaApi } from '../funciones/api.ts';
 import { formatoFechaYHora } from '../funciones/funciones';
+import AlertaMensaje from '../components/AlertaMensaje.vue';
 formatoFechaYHora
 const props = defineProps({
   seccion: {
@@ -9,8 +10,10 @@ const props = defineProps({
     default: ''
   },
 })
+const alertaMsj = ref<string>('')
 const dialog = ref(false)
 const asisDate = ref(formatoFechaYHora(new Date, 'fecha'))
+const asisHor = ref(showTime())
 const estudiantes = ref([
   {
     ced_alum: "",
@@ -21,7 +24,7 @@ const estudiantes = ref([
     snom_alum: "",
   },
 ])
-const asistencias = ref([])
+const inasistencias = ref([])
 function cargaInicial() {
   if (props.seccion) {
     brunaApi({ s: 'estudiantes' }, 'ano=' + props.seccion)
@@ -34,12 +37,36 @@ function cargaInicial() {
     })
   }
 }
-onMounted(() => {
-	cargaInicial();
-});
+function guardarInasistencias() {
+  if (inasistencias.value.length) {
+    brunaApi({ s: 'registrarInasistencias' }, 'alum=' + inasistencias.value + "&fec=" + asisDate.value + "&hor=" + asisHor.value)
+    .then((res:any) => {
+      if (res.data.r) {
+        dialog.value = false
+        alertaMsj.value = res.data.e
+      } else {
+        alertaMsj.value = res.data.e
+      }
+    }).catch(() => {
+      alertaMsj.value = res.data.e
+    })
+  }
+}
+function showTime(){
+  const myDate = new Date();
+  let hours = myDate.getHours();
+  let minutes = myDate.getMinutes();
+  if (hours < 10) hours = '0' + hours;
+  if (minutes < 10) minutes = '0' + minutes;
+  return hours+ ":" +minutes
+}
+watch(()=>dialog.value, ()=>{
+  cargaInicial();
+})
 </script>
 <template>
   <v-row justify="center">
+    <AlertaMensaje :mensaje="alertaMsj" />
     <v-dialog
       v-model="dialog"
       scrollable
@@ -62,8 +89,20 @@ onMounted(() => {
         <v-card>
           <v-card-text style="height: 300px;">
             <v-text-field v-model="asisDate" type="date" label="Selecciona la fecha de la asistencia" />
+            <v-text-field
+              v-model="asisHor"
+              label="Hora de la asistencia"
+              type="time"
+              persistent-hint
+              class="mb-3"
+            />
+            <p class="text-caption">
+              <span class="d-block text-caption">
+                Marque los inasistentes
+              </span>
+            </p>
             <v-checkbox
-              v-model="asistencias"
+              v-model="inasistencias"
               v-for="estudiante in estudiantes"
               :key="estudiante.id_estd"
               :label="`${estudiante.pnom_alum} ${estudiante.snom_alum || ''} ${estudiante.pape_alum} ${estudiante.sape_alum || ''}`"
@@ -85,7 +124,7 @@ onMounted(() => {
               variant="tonal"
               color="primario"
               text="Guardar"
-              @click="isActive.value = false"
+              @click="guardarInasistencias"
             />
           </v-card-actions>
         </v-card>
